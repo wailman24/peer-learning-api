0fab15e8f60e566e461def5dacf47b4f6a7146e9
wailman24 <wailmansour24@gmail.com>
2025-04-11 21:58:48 +0100

    add the websocket

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -48,9 +48,9 @@ return [
 
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
-            'secret' => env('PUSHER_APP_SECRET'),
-            'app_id' => env('PUSHER_APP_ID'),
+            'key' => env('REVERB_APP_KEY'),
+            'secret' => env('REVERB_APP_SECRET'),
+            'app_id' => env('REVERB_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST', '127.0.0.1'),
